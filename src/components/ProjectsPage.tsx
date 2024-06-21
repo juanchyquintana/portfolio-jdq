34e55997beed8e6b42e902ae59d428854e397315
Juan Diego <quintanajuanchy25@gmail.com>
2024-06-21 13:21:05 -0300

    se agregaron los proyectos en su seccion

--- src/components/ProjectsPage.tsx
+++ src/components/ProjectsPage.tsx
@@ -1,9 +1,12 @@
-
+import { projects } from "../data/projects";
+import CardProject from "./CardProject";
 
 export default function ProjectsPage() {
   return (
-    <div>
-      hoola
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-5">
+      {projects.map((project) => (
+        <CardProject key={project.id} project={project} />
+      ))}
     </div>
-  )
+  );
 }
--- src/layouts/Layouts.tsx
+++ src/layouts/Layouts.tsx
@@ -1,13 +1,17 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import { useMemo } from "react";
 
 export default function Layouts() {
+  const { pathname } = useLocation();
+  const isProjectsPage = useMemo(() => pathname === "/projects", [pathname])
+
   return (
     <>
       <Header />
 
-      <main className="mx-auto flex max-w-[700px] items-center justify-between gap-3 px-4 py-3 md:px-6">
+      <main className={`mx-auto flex ${isProjectsPage ? "max-w-[950px]" : "max-w-[700px]"} items-center justify-between gap-3 px-4 py-3 md:px-6`}>
         <div className="">
           <Outlet />
         </div>

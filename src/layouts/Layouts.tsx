5496836d55b40fb75f01879a9aeb3862b9f619f2
Juan Diego <quintanajuanchy25@gmail.com>
2024-06-23 17:41:35 -0300

    se agregó animaciones en la pagina principal

--- src/layouts/Layouts.tsx
+++ src/layouts/Layouts.tsx
@@ -15,7 +15,7 @@ export default function Layouts() {
       <main
         className={`mx-auto flex ${
           isProjectsPage ? "max-w-[950px]" : "max-w-[700px]"
-        } items-center justify-between gap-3 py-3 md:px-6 md:my-5 ${
+        } items-center justify-between gap-3 px-4 py-3 md:px-6 md:my-5 ${
           isAboutMePage ? "md:max-w-[1300px]" : "max-w-[700px]"
         }`}
       >

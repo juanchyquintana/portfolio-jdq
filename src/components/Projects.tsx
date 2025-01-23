b1e1ad62e9c4d3048698de6e929e41f2fae5d276
Juan Diego <quintanajuanchy25@gmail.com>
2025-01-23 11:30:42 -0300

    modified design of projects card and add a modal details about of projects

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,5 +1,4 @@
 import { ProjectsType } from "../types";
-import profilePhoto from "../img/Perfil_Foto_Juan.jpeg";
 import { Link } from "react-router-dom";
 
 type ProjectsProps = {
@@ -10,12 +9,15 @@ function Projects({ project }: ProjectsProps) {
   return (
     <>
       <section className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-        <img
-          className="object-cover w-full rounded-t-lg h-40 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg"
-          src={profilePhoto}
-          alt={project.name}
-        />
-        <div className="flex flex-col justify-between p-4 leading-normal">
+        <div className="h-full">
+          <img
+            className="object-cover w-full h-48 md:h-48 md:w-48 rounded-t-lg md:rounded-none md:rounded-s-lg"
+            src={project.image}
+            alt={project.name}
+          />
+        </div>
+
+        <div className="flex flex-col justify-between p-4 leading-normal border-l-2 border-blue-700">
           <div>
             <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
               {project.name}
@@ -28,13 +30,12 @@ function Projects({ project }: ProjectsProps) {
             </p>
           </div>
 
-          <div className="w-full text-center font-bold bg-blue-700 text-white p-2 rounded-lg uppercase hover:bg-blue-500">
-            <Link
-              to={`/projects/${project.id}`}
-            >
-              Ver más
-            </Link>
-          </div>
+          <Link
+            className="w-full text-center font-bold bg-blue-700 text-white p-2 rounded-lg uppercase hover:bg-blue-500"
+            to={`/projects/${project.id}`}
+          >
+            Ver más
+          </Link>
         </div>
       </section>
     </>
